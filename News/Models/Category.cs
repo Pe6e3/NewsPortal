43edc5c7daf576d1c08fb89b25ea0b2872288037
Pe6e3 <xpe6e3@gmail.com>
2023-04-21 19:34:57 +0600

    Add project files.

---
+++ News/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace News.Models
+{
+    public class Category
+    {
+
+        [Key]
+        public int CatehoryId { get; set; }
+
+        [Display(Name = "Название категории")]
+        public string? CategoryName { get; set; }
+        public List<Post>? Posts { get; set; }
+    }
+}
---
+++ News/Models/Post.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace News.Models
+{
+    public class Post
+    {
+        [Key]
+        public int PostId { get; set; }
+        [Display(Name="Заголовок новости")]
+        public string? PostTitle { get; set; }
+
+
+        [Display(Name = "Текст новости")]
+        public string? PostBody { get; set; }
+
+
+        [Display(Name = "Категория новости")]
+        public int CategoryId { get; set; }
+        public virtual Category?  Category { get; set; }
+
+
+
+        [Display(Name = "Дата публикации")]
+        [DisplayFormat(DataFormatString = "yyyy-MM-ddTHH:mm", ApplyFormatInEditMode = true)]
+        public DateTime PostDate { get; set; }
+    }
+}
